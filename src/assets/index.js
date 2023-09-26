d23c041655b61c20474a5a8971261235f709dc69
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-26 21:36:41 +0500

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,1 @@
+export { default as logoImg } from "./images/logo.svg";
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+// styles
+import "./scss/main.scss";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
