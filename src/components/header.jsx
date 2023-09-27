7222669235b2c0ab14e02c3801ba21c0e4dfec50
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-27 18:01:58 +0500

    Make header  responsive for all screens

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,36 +1,50 @@
 import { Link } from "react-scroll";
-import { logoImg } from "../assets";
+import { iconClose, iconMenu, logoImg } from "../assets";
+import { useState } from "react";
 
 const Header = () => {
+  const [isSticky, setIsSticky] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 80) setIsSticky(true);
+    else setIsSticky(false);
+  });
+
   return (
-    <header className="header">
+    <header className={`header ${isSticky ? "header--sticky" : ""}`}>
       <div className="header__container container">
-        <a className="logo header__logo" href="/">
-          <img className="logo__img" src={logoImg} alt="logo" width={97} height={49} />
-        </a>
+        <div className="header__left">
+          <button className="button header__nav-toggler" onClick={() => setIsNavOpen(true)}>
+            <img className="header__nav-toggler-img" src={iconMenu} alt="icon menu" width={52} height={50} />
+          </button>
+          <a className="logo header__logo" href="/">
+            <img className="logo__img" src={logoImg} alt="logo" width={97} height={49} />
+          </a>
+        </div>
 
         <div className="header__actions">
-          <nav className="nav">
-            <ul className="nav__list">
-              <li className="nav__item">
-                <Link className="nav__link" to="features" spy={true} smooth={true}>
-                  Нима учун айнан биз?
-                </Link>
-              </li>
+          <nav className={`nav ${isNavOpen ? "nav--open" : ""}`}>
+            <button className="button nav__close-btn" onClick={() => setIsNavOpen(false)}>
+              <img className="nav__close-btn-img" src={iconClose} alt="icon close" width={36} height={36} />
+            </button>
+            <Link className="nav__link" activeClass="nav__link--active" to="features" spy={true} smooth={true}>
+              Нима учун айнан биз?
+            </Link>
 
-              <li className="nav__item">
-                <Link className="nav__link" to="services" spy={true} smooth={true}>
-                  Хизматлар
-                </Link>
-              </li>
-              <li className="nav__item">
-                <Link className="nav__link" to="features" spy={true} smooth={true}>
-                  Контакт
-                </Link>
-              </li>
-            </ul>
+            <Link className="nav__link" activeClass="nav__link--active" to="services" spy={true} smooth={true}>
+              Хизматлар
+            </Link>
+
+            <Link className="nav__link" activeClass="nav__link--active" to="faq" spy={true} smooth={true}>
+              FAQ
+            </Link>
+
+            <Link className="nav__link" activeClass="nav__link--active" to="contact" spy={true} smooth={true}>
+              Боғланиш
+            </Link>
           </nav>
-          <a className="button--teal header__order-btn">Буюртма қилиш</a>
+          <a className="button--teal-mini header__order-btn">Буюртма қилиш</a>
         </div>
       </div>
     </header>
