63c8ab8eafa08edcb0a75ef2e82c78d798bca100
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-27 23:14:19 +0500

    Make responsive contact for all screens

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <section className="contact">
       <h2 className="contact__title">Боғланиш</h2>
-      <div className="contact__links">
+      <div className="contact__container container">
         {socials.map(({ link, icon, text, alt, id }) => (
           <a key={id} className="contact__link button--rect" href={link} target="_blank">
             <img className="contact__link-icon" src={icon} alt={alt} width={24} height={24} />
