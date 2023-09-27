a4f63e7e9df1c0a6a8c14759c8b550ed9f129a35
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-27 12:44:58 +0500

    Update hero and services

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export { default as App } from "./app";
 export { default as Header } from "./header";
 export { default as Hero } from "./hero";
+export { default as Services } from "./services";
+export { default as ServiceCard } from "./service-card";
+export { default as Contact } from "./contact";
+export { default as Faq } from "./faq";
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,11 +1,11 @@
-import ServiceCard from "./service-card";
 import { services } from "../helpers/constants";
+import { ServiceCard } from ".";
 
 const Services = () => {
   return (
     <section className="services" id="services">
       <h2 className="services__title">Хизматлар</h2>
-      <div className="services__container">
+      <div className="container services__container">
         {services.map(service => (
           <ServiceCard key={service.id} {...service} />
         ))}
