30b7181dd6f9a1e6ff7a5ce1159b29f80dfb8b60
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-27 22:39:56 +0500

    Make responsive services for all screens

--- src/components/service-card.jsx
+++ src/components/service-card.jsx
@@ -9,7 +9,13 @@ const ServiceCard = ({ name, desc, price, time, iconName }) => {
       <p className="service__time">{time}</p>
       <a className={`service__link service__link--${iconName} button--teal button--with-icon`} href="#">
         <span className="service__link-text button__text">Батафсил маълумот</span>
-        <img className="button__icon" src={iconArrowRight} alt="arrow right icon" width={9} height={16} />
+        <img
+          className="button__icon service__link-icon"
+          src={iconArrowRight}
+          alt="arrow right icon"
+          width={9}
+          height={16}
+        />
       </a>
     </div>
   );
