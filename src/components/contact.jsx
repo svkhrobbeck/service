cf46e5b59558ce8aff34a95cef5fd54e236d10e4
svkhrobbeck <svkhrobbeck@gmail.com>
2023-09-27 16:19:19 +0500

    Update contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,8 +1,17 @@
+import { socials } from "../helpers/constants";
+
 const Contact = () => {
   return (
     <section className="contact">
-      <h2 className="contact__title">Контактлар</h2>
-      
+      <h2 className="contact__title">Боғланиш</h2>
+      <div className="contact__links">
+        {socials.map(({ link, icon, text, alt, id }) => (
+          <a key={id} className="contact__link button--rect" href={link} target="_blank">
+            <img className="contact__link-icon" src={icon} alt={alt} width={24} height={24} />
+            <span className="contact__link-text">{text}</span>
+          </a>
+        ))}
+      </div>
     </section>
   );
 };
